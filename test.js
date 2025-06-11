ae7defdb2b3ea9a2727b83bccb5391ce0ec29c84
Hazran <hazranhafiz.ahmad@gmail.com>
2025-06-11 13:27:13 +0800

    Change to Mocha

--- test.js
+++ test.js
@@ -1,8 +1,7 @@
-// test.js
 const assert = require('assert');
 
 describe('Sample Test', function () {
-  it('should return true', function () {
-    assert.strictEqual(true, true);
+  it('should pass', function () {
+    assert.strictEqual(1 + 1, 2);
   });
 });
